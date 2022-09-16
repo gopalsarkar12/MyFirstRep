dd7b6568277fec9cdcd2f1c490a59d795fefffe9
gopalsarkar12 <110389254+gopalsarkar12@users.noreply.github.com>
2022-09-16 18:50:26 +0530

    Add files via upload

---
+++ current time.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[45]:
+
+
+import datetime
+now = datetime.datetime.now()
+print ("Current Date and Time is: ")
+print (now.strftime("%D  %R"))
+
