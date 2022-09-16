10fe455bda12ebd11dcbb56f3621295bd172be9d
gopalsarkar12 <110389254+gopalsarkar12@users.noreply.github.com>
2022-09-16 18:47:10 +0530

    Update clock timer.py

--- clock timer.py
+++ clock timer.py
@@ -1,48 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
-
-
 import time
-
-
-# In[25]:
-
 
 result= time.localtime()
 
-
-# In[26]:
-
-
 print(result)
-
-
-# In[ ]:
-
 
 while True:
     result=time.localtime()
     if result.tm_hour==16 and result.tm_min==11:
         print("Hey! Dude It's Time to Wake-up")
         break
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
